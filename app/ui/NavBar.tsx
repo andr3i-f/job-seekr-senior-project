7a636b2df826f53bd871ab0e79b18a0030cf0ad6
andr3if <andrei.florea003@gmail.com>
2025-04-20 18:51:31 -0700

    added basic landing page, navbar

---
+++ app/ui/NavBar.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+import { JSX } from "react";
+
+export default function NavBar(): JSX.Element {
+  return (
+    <Box
+      width={"100vw"}
+      height={"5vh"}
+      sx={{ backgroundColor: "purple" }}
+    ></Box>
+  );
+}

c73d7d5ee661ece04b6779b5e7b63ab4dc455470
andr3if <andrei.florea003@gmail.com>
2025-04-20 19:17:50 -0700

    updated navbar, landing page button

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,14 @@
 import { Box, Button, Stack, Typography } from "@mui/material";
-import NavBar from "./ui/NavBar";
 
 export default function Home() {
   return (
     <Box
       sx={{
         width: "100vw",
-        height: "200vh",
+        height: "200vh"
       }}
     >
       <Stack direction={"column"} alignItems={"center"}>
-        <NavBar />
         <Typography fontSize={"64px"} sx={{ mt: "25vh" }}>
           find your next job.
         </Typography>
@@ -19,12 +17,15 @@ export default function Home() {
         </Typography>
         <Button
           variant="contained"
-          sx={{ mt: "2vh", backgroundColor: "purple" }}
+          sx={{ mt: "2vh", backgroundColor: "white", color: "#64558f", textTransform: "lowercase", borderRadius: "13px" }}
         >
           get started
         </Button>
-        <Typography fontSize={"64px"} sx={{ mt: "75vh" }}>
+        <Typography fontSize={"64px"} sx={{ mt: "65vh" }}>
           how does it work?
+        </Typography>
+        <Typography fontSize={"32px"} sx={{ mt: "2vh" }}>
+          jobseekr works by scraping the internet for new openings
         </Typography>
       </Stack>
     </Box>
--- app/ui/NavBar.tsx
+++ app/ui/NavBar.tsx
@@ -1,12 +1,21 @@
-import { Box } from "@mui/material";
+import { AccountCircle } from "@mui/icons-material";
+import { Box, Stack } from "@mui/material";
 import { JSX } from "react";
 
 export default function NavBar(): JSX.Element {
   return (
     <Box
       width={"100vw"}
-      height={"5vh"}
-      sx={{ backgroundColor: "purple" }}
-    ></Box>
+      height={"6vh"}
+      sx={{
+        background: "#372483",
+        boxShadow: "2",
+        borderBottomRightRadius: "10px",
+        borderBottomLeftRadius: "10px",
+      }}>
+        <Stack direction={"row"} width={"100vw"} height={"100%"} justifyContent="flex-end" alignItems="center" px={2}>
+            <AccountCircle sx={{ fontSize: "32px" }} />
+        </Stack>
+      </Box>
   );
 }

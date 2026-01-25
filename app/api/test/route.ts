c161e51fac3c5d18cd296145f855b781f1e0ce7a
andr3if <andrei.florea003@gmail.com>
2026-01-24 18:26:54 -0800

    cleaned impl

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -3,6 +3,9 @@ import { createClient } from "@/lib/supabase/server";
 import axios from "axios";
 
 export async function GET(req: Request) {
+  const { searchParams } = new URL(req.url);
+
+  const test_name = searchParams.get("test");
   const supabase = await createClient();
 
   const { data, error } = await supabase.auth.getSession();
@@ -11,17 +14,17 @@ export async function GET(req: Request) {
     return NextResponse.json({ error: "Not authenticated" }, { status: 400 });
   }
 
-  return NextResponse.json(
-    { message: "You are authenticated!" },
-    { status: 200 },
+  const { data: axiosData } = await axios.get(
+    process.env.JOB_SEEKR_JOB_API! + "/test/specific_test",
+    { params: { test_name: test_name } },
   );
+  return NextResponse.json({ message: axiosData }, { status: 200 });
 }
 
 export async function POST(req: Request) {
   const body = await req.json();
-  console.log(body["test"])
   if (!body || !("test" in body)) {
-    return NextResponse.json({ error: "'test' key missing from body!" })
+    return NextResponse.json({ error: "'test' key missing from body!" });
   }
 
   const supabase = await createClient();
@@ -31,7 +34,14 @@ export async function POST(req: Request) {
     return NextResponse.json({ error: "Not authenticated" }, { status: 400 });
   }
 
-  const { data: axiosData } = await axios.post(process.env.JOB_SEEKR_JOB_API! + "/test/add", { test_name: body["test"] }, { headers: { "Content-Type": "application/json" } })
-  
-  return NextResponse.json({ message: axiosData }, {status: axiosData.status})
+  const { data: axiosData } = await axios.post(
+    process.env.JOB_SEEKR_JOB_API! + "/test/add",
+    { test_name: body["test"] },
+    { headers: { "Content-Type": "application/json" } },
+  );
+
+  return NextResponse.json(
+    { message: axiosData },
+    { status: axiosData.status },
+  );
 }
--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,36 +1,48 @@
 "use client";
 
-import { Box, Button, Stack, TextField } from "@mui/material";
+import { Box, Button, Stack, TextField, Typography } from "@mui/material";
 import { NAVBAR_HEIGHT_IN_VH } from "@/constants/layout";
 import { useState } from "react";
 import axios from "axios";
 
 export default function TestPage() {
-    const [testName, setTestName] = useState("");
+  const [testName, setTestName] = useState("");
+  const [returnValue, setReturnValue] = useState("");
+  const createTestEntry = async () => {
+    const res = await axios.post("/api/test", { test: testName });
+    setReturnValue(res.data.message);
+  };
 
-    const createTestEntry = async () => {
-        const res = await axios.post("http://localhost:3000/api/test", { "test": testName })
-        console.log(res)
-    }
+  const getTestEntry = async () => {
+    const res = await axios.get("/api/test", { params: { test: testName } });
+    setReturnValue(res.data.message.test_name);
+  };
 
-    return (
-        <Box
-        sx={{
-            width: "100vw",
-            height: `${100 - NAVBAR_HEIGHT_IN_VH}vh`,
-        }}
-        >
-        <Stack
-            direction={"column"}
-            justifyContent={"center"}
-            alignItems={"center"}
-            height={"100%"}
-        >
-            <TextField onChange={(e) => setTestName(e.target.value)} defaultValue={"enter test value..."}/>
-            <Button variant="contained" onClick={() => createTestEntry()}>
-            Login with Google
-            </Button>
-        </Stack>
-        </Box>
-    )
+  return (
+    <Box
+      sx={{
+        width: "100vw",
+        height: `${100 - NAVBAR_HEIGHT_IN_VH}vh`,
+      }}
+    >
+      <Stack
+        direction={"column"}
+        justifyContent={"center"}
+        alignItems={"center"}
+        height={"100%"}
+      >
+        <TextField
+          onChange={(e) => setTestName(e.target.value)}
+          defaultValue={"enter test value..."}
+        />
+        <Button variant="contained" onClick={() => createTestEntry()}>
+          send data
+        </Button>
+        <Button variant="contained" onClick={() => getTestEntry()}>
+          get data
+        </Button>
+        <Typography variant="h3">{returnValue}</Typography>
+      </Stack>
+    </Box>
+  );
 }
